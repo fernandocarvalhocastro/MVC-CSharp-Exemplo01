cba35ff89b42da8209b728b50d30bfd5e990c01b
logonrm <logonrm@fiap.com.br>
2017-08-14 20:34:47 -0300

    Commit inicial

---
+++ Fiap.Exemplo01.MVC/Fiap.Exemplo01.MVC/Controllers/AlunoController.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.Exemplo01.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo01.MVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        //Action para abrir o formulário
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        //Action para cadastrar as informações do formulário
+        [HttpPost]
+        public ActionResult Salvar(Aluno aluno)
+        {
+            ViewBag.nomeAluno = aluno.Nome;
+            ViewBag.dataAluno = aluno.DataNascimento;
+            ViewBag.salarioAluno = aluno.Salario;
+            return View();
+            //return Content("Gravou!" + aluno.Nome + " - " + aluno.DataNascimento.ToString("dd/MM/yyyy") + " - " + aluno.Salario); //Texto
+        }
+    }
+}
---
+++ Fiap.Exemplo01.MVC/Fiap.Exemplo01.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fiap.Exemplo01.MVC.MvcApplication" Language="C#" %>
